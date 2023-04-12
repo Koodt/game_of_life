858cbb1d7dce058db5634e32f96c3a21ba939f54
Konstantin Eremin <kod-t@mail.ru>
2023-04-12 12:26:50 +0300

    Add tests, fix ci.yml, add files

--- cloud/main.tf
+++ cloud/main.tf
@@ -104,7 +104,9 @@ pip3 install \
   psycopg2 \
   flake8 \
   black \
-  isort
+  isort \
+  coverage \
+  pytest
 
 
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
